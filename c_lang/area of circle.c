334116a71963f06ee04d3a2e6c079d37d0e4f071
Aayushi <aayushi24cg@gmail.com>
2025-07-14 21:54:55 -0700

    c_lang

---
+++ c_lang/area of circle.c
@@ -0,0 +1,14 @@
+// area of the circle//
+#include<stdio.h>
+main()
+{
+	int r;
+	float pi=3.14;
+	printf("enter the value of r");
+	scanf("%d",&r);
+	float area=pi*r*r;
+	printf("area of the circle =%f",area);
+	
+	
+	
+}
